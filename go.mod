ea8a51c7e791c3b961a71b5c179b60b1cb975cbc
Nassim Abedi <nassimabedi@gmail.com>
2020-10-22 20:06:39 +0330

    login inaccessble

---
+++ go.mod
@@ -0,0 +1,5 @@
+module webscraping
+
+go 1.14
+
+require github.com/PuerkitoBio/goquery v1.6.0 // indirect
--- main.go
+++ main.go
@@ -3,10 +3,11 @@ package main
 import (
     "fmt"
     "net/http"
-    "log"
+   "log"
 
     // import third party libraries
     "github.com/PuerkitoBio/goquery"
+    "strings"
 )
 
 func main() {
@@ -17,7 +18,20 @@ func main() {
 
 
 func metaScrape(w http.ResponseWriter, r *http.Request) {
-    doc, err := goquery.NewDocument("http://jonathanmh.com")
+    //pageURL := "https://www.w3schools.com/html/tryit.asp?filename=tryhtml_headings"
+    //pageURL := "http://jonathanmh.com"
+    pageURL := "https://developer.mozilla.org/en-US/docs/Web/HTML/Element/Heading_Elements" //links
+    // pageURL := "http://metalsucks.net" //inaccible code 403
+    //error handing
+    res, err := http.Get(pageURL)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer res.Body.Close()
+    if res.StatusCode != 200 {
+        log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+    }
+    doc, err := goquery.NewDocument(pageURL)
     if err != nil {
         log.Fatal(err)
     }
@@ -36,7 +50,64 @@ func metaScrape(w http.ResponseWriter, r *http.Request) {
     })
     fmt.Printf("Page Title: '%s'\n", pageTitle)
     fmt.Printf("Meta Description: '%s'\n", metaDescription)
+
+    fmt.Println("================heading")
+    //find heading : six level
+    doc.Find("h1").Each(func(i int, h1 *goquery.Selection) {
+         fmt.Println(i)
+    })
+
+    fmt.Println("================link+++++++++++++++++++++++")
+    //internal and external links
+    doc.Find("body a").Each(func(index int, item *goquery.Selection) {
+        linkTag := item
+        link, _ := linkTag.Attr("href")
+        linkText := linkTag.Text()
+	checkExtrernal := strings.HasPrefix(link, "http")
+	external := "true"
+	domain := ""
+	if checkExtrernal {
+		external = "true"
+
+	} else {
+		external = "false"
+		domain = r.URL.Host
+		fmt.Println(domain)
+		return 
+
+	}
+
+	
+
+	res, err := http.Get(link)
+        if err != nil {
+            log.Fatal(err)
+        }
+        defer res.Body.Close()
+
+        fmt.Printf("Link #%d: '%s' - '%s'-  '%s', '%s', '%s' \n", index, linkText, link,external, strings.HasPrefix(link, "http"), res.StatusCode )
+    })
+
+    //login
+    doc.Find("html body").Each(func(_ int, item *goquery.Selection) {
+        // for debug.
+        println(item.Size()) // return 1
+
+       // if len(s.Nodes) > 0 && s.Nodes[0].Type == html.ElementNode {
+         //   println(s.Nodes[0].Data)
+        //}
+	if( item.AttrOr("name","") == "password") {
+		fmt.Println("here is login form")
+	}
+    })
+
 }
+
+
+//func findHeading(doc) {
+
+
+//}
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
