b1894bf98346f66e7d0ab15f9486429d0a49359d
Nassim Abedi <nassimabedi@gmail.com>
2020-10-24 00:05:10 +0330

    added search

--- web/main.go
+++ web/main.go
@@ -1,12 +1,68 @@
 package main
 
 import (
+  "fmt"
+  "github.com/PuerkitoBio/goquery"
+  "log"
   "net/http"
 
   "github.com/gin-gonic/gin"
 )
 
 var router *gin.Engine
+
+
+
+
+
+
+func search (c *gin.Context) {
+   //link := c.Param("q")
+   //link ,err :=  c.Params.Get("q")
+  pageURL := c.Query("q")
+
+  fmt.Println("==================================")
+    fmt.Println(pageURL)
+   //fmt.Println(err)
+
+
+  res, err := http.Get(pageURL)
+  if err != nil {
+    log.Fatal(err)
+  }
+  defer res.Body.Close()
+  if res.StatusCode != 200 {
+    log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+  }
+  doc, err := goquery.NewDocument(pageURL)
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  var pageTitle string
+
+  // use CSS selector found with the browser inspector
+  // for each, use index and item
+  pageTitle = doc.Find("title").Contents().Text()
+  fmt.Println(pageTitle)
+
+
+    // Call the HTML method of the Context to render a template
+    c.HTML(
+      // Set the HTTP status to 200 (OK)
+      http.StatusOK,
+      // Use the index.html template
+      "index.html",
+      // Pass the data that the page uses
+      gin.H{
+        "title":   "Home Page",
+        "pageTitle" : pageTitle,
+        //"payload": articles,
+      },
+    )
+
+
+}
 
 func main() {
 
@@ -35,6 +91,8 @@ func main() {
     )
 
   })
+
+  router.GET("/search", search )
 
   // Start serving the application
   router.Run()
