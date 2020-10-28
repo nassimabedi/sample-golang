131fefa3b071493f321bfe06636ad9d14c702083
Nassim Abedi <nassimabedi@gmail.com>
2020-10-28 18:59:27 +0330

    get object info

--- throughput/main.go
+++ throughput/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"sync"
@@ -13,6 +14,10 @@ type TestObject struct {
 	ObjectIds []int   `json:"object_ids"`
 }
 
+type ObjectInfo struct {
+	Id int   `json:"id"`
+	Online bool   `json:"online"`
+}
 
 
 
@@ -39,7 +44,32 @@ func getObjectInfo(objId int, wg *sync.WaitGroup) {
 	//	}
 	//}()
 
-	fmt.Println(objId)
+	fmt.Println("==========", objId)
+	//resp, err := http.Get("http://localhost:9010/objects/"+ string(objId))
+	//resp, err := http.Get("http://localhost:9010/objects/"+objId)
+	resp, err := http.Get(fmt.Sprintf("http://localhost:9010/objects/%d", objId))
+
+	if err != nil {
+		fmt.Println("errr1111111111111111111111111111")
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		fmt.Println("errr2222222222222222222")
+		panic(err)
+	}
+
+	fmt.Println(string(body))
+	var t ObjectInfo
+	err = json.Unmarshal(body, &t)
+	if err != nil {
+		panic(err)
+	}
+	log.Println(t.Online)
 }
 
 func CallBack(w http.ResponseWriter, r *http.Request) {
